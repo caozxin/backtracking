db9432f78a7afab3d1a0d30b9d981baeee782614
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-03-28 07:05:51 -0400

    Update 257. Binary Tree Paths.py

--- 257. Binary Tree Paths.py
+++ 257. Binary Tree Paths.py
@@ -4,6 +4,9 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+## my initial approach:
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         
@@ -27,6 +30,35 @@ class Solution:
             # print("af", node.val)
             
             
+        res = []
+        dfs_path(root, [])
+        print(res)
+        return res
+
+
+## improved approach:
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        
+        if not root:
+            return []
+
+        def dfs_path(node, path):
+            if not node: # exist strategy
+                return # we do not need to return any child value to parent level in this case. 
+
+            path.append(str(node.val))  # Convert to string for proper path formatting
+            
+            # ONLY If it's a leaf node, add the path to results
+            if not node.left and not node.right:
+                res.append("->".join(path))  # Store path as string
+
+            left = dfs_path(node.left, path)
+            right = dfs_path(node.right, path)
+
+            path.pop()  ## when you finish processing the current node, Remove it =last element when backtracking
+            
+
         res = []
         dfs_path(root, [])
         print(res)
