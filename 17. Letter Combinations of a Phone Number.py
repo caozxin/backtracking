c75e9908003aedf05df879e162c937b97e229dd5
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-03-30 07:04:53 -0400

    Update 17. Letter Combinations of a Phone Number.py

--- 17. Letter Combinations of a Phone Number.py
+++ 17. Letter Combinations of a Phone Number.py
@@ -6,23 +6,40 @@ class Solution:
         if not digits:
             return []
         len_of_solu = len(digits)
-        digits_lst = list(digits)
-        print(digits_lst)
+        # digits_lst = list(digits)
+        # print(digits_lst)
 
         #mapping for num to letters:
         num_char = {
             '2': ['a','b', 'c'],
             '3': ['d', 'e', 'f']
         }
+        KEYBOARD = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
 
-        # print(num_char)
-        # print(num_char['2'])
 
         def dfs_backtrack(start_idx:int, path:List[str]):
+
             if start_idx == len_of_solu:
                 res.append("".join(path))
-                print("res: ", res)
-            return 
-
-            for letter in num_char['2']:
+                # print("res: ", res)
+                return 
+            next_num = digits[start_idx] # the start_idx matches the order of the nums! 
+            for letter in KEYBOARD[next_num]:
                 path.append(letter)
+                print(path)
+                dfs_backtrack(start_idx + 1, path) # this traversal is pre-order == DFS
+                path.pop()
+
+        res = []
+
+        dfs_backtrack(0,[])
+        return res
