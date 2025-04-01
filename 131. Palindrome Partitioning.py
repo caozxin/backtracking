b810691edd60b6d8571e5410195b6618eeb1a4e2
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-04-01 07:44:34 -0400

    Update 131. Palindrome Partitioning.py

--- 131. Palindrome Partitioning.py
+++ 131. Palindrome Partitioning.py
@@ -1,19 +1,25 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        def dfs_backtrack(start_idx, path):
-            if start_idx == n: # if the substring has the len of input string, then it reachs the leaf.
-                if path is palindrome: 
-                    res.append("".join(path))
+        n = len(s)
+        def if_palindrome(word):
+            return word == word[::-1]
+
+        def dfs_backtrack(start, path):
+            # print("  dfs")
+            if start == n: # if the substring has the len of input string, then it reachs the leaf.
+                # res.append("".join(path))
+                res.append(path[:])
                 return # always return blank in backtracking. 
 
-            for edge in get_edges(start_idx):
-                if not is_subtring(edge):
-                    continue
+            for end in range(start+1, n+1): #first you need to gather all defined edges, in this case substrings
+                substr = s[start:end]
+                # print(start, end, substr)
+                # dfs_backtrack(end, path) # in each recursive call, end_idx pass in as start_idx
+                if if_palindrome(substr):
+                    # path.append(edge)
+                    dfs_backtrack(end, path + [substr])
+                    # path.pop() # this should be right after dfs(). 
 
-                path.add(edge)
-                dfs_backtrack(start_idx + len(edge), path)
-                path.pop()
-
-        res = set() # avoid duplication. 
+        res = [] 
         dfs_backtrack(0, [])
         return res
