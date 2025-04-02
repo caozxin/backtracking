2a4aa52a9f84b6a4335d7f29e5da14f8eb55038a
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-04-02 05:59:46 -0400

    Update 22. Generate Parentheses.py

--- 22. Generate Parentheses.py
+++ 22. Generate Parentheses.py
@@ -30,3 +30,34 @@ class Solution:
 
         dfs_backtrack(0, [])
         return res
+
+
+
+##### this is a better run time version:
+from typing import List
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+
+        def backtrack(path, open_count, close_count):
+            # Base case: If the length of the path is 2 * n, we have a valid sequence
+            if len(path) == 2 * n:
+                res.append("".join(path))
+                return
+
+            # Add '(' if we haven't used all `n` opening brackets
+            if open_count < n:
+                path.append('(')
+                backtrack(path, open_count + 1, close_count)
+                path.pop()  # Backtrack
+
+            # Add ')' if we haven't closed more than opened brackets
+            if close_count < open_count:
+                path.append(')')
+                backtrack(path, open_count, close_count + 1)
+                path.pop()  # Backtrack
+
+        # Start backtracking
+        backtrack([], 0, 0)
+        return res
