d9b7eda4bdbf13d536bbe96e729c1595ae4749ae
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-04-04 13:19:45 -0400

    Update 46. Permutations.py

--- 46. Permutations.py
+++ 46. Permutations.py
@@ -8,15 +8,25 @@ class Solution:
         def dfs_backtrack(start_idx, path):
             if start_idx == n:
                 res.append(path) # note: path here is a list. 
+                print("res", res)
                 return # backtracking always return blank. 
-            get_edges = nums
-            for edge in get_edges(start_idx):
+            # get_edges = nums
 
-                #if pruning is needed?
-
-                path.add(edge)
-                dfs_backtrack(start_idx + len(edge), path[:])
+            for edge in nums[start_idx:]:
+                #if pruning is needed:
+                # if edge == path.
+                # if not is_valid(edge):
+                #     continue
+                print(edge, path)
+                path.append(edge)
+                print("reset", nums[start_idx:])
+                # exit()
+                dfs_backtrack(start_idx + 1, path[:]) #len(edge)
+                print(path)
                 path.pop()
+                
+                # exit()
+
 
         dfs_backtrack(0, [])
         return res
